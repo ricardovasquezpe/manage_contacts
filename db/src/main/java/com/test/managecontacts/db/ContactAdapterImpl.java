7cd460b49f9bd7c1dd5ff316c5b60ba5aa59d2c3
Ricardo Vasquez <ricardo.vasquez.pe@gmail.com>
2021-04-03 16:05:05 -0500

    exception

--- db/src/main/java/com/test/managecontacts/db/ContactAdapterImpl.java
+++ db/src/main/java/com/test/managecontacts/db/ContactAdapterImpl.java
@@ -5,9 +5,11 @@ import com.test.managecontacts.db.mapper.ContactMapperService;
 import com.test.managecontacts.db.model.ContactModel;
 import com.test.managecontacts.usecases.contact.ContactAdapter;
 import com.test.managecontacts.entity.Contact;
+import com.test.managecontacts.usecases.contact.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 public class ContactAdapterImpl implements ContactAdapter {
 
@@ -37,13 +39,21 @@ public class ContactAdapterImpl implements ContactAdapter {
 
     @Override
     public Contact getContactById(Long id) {
-        ContactModel result = this.contactRepository.findById(id).get();
-        return ContactMapper.convertModelToEntity(result);
+        Optional<ContactModel> result = this.contactRepository.findById(id);
+        if(!result.isPresent()){
+            throw new ResourceNotFoundException("Contact not found");
+        }
+        ContactModel model = result.get();
+        return ContactMapper.convertModelToEntity(model);
     }
 
     @Override
     public Contact updateContactById(Long id, Contact contact) {
-        ContactModel model = this.contactRepository.findById(id).get();
+        Optional<ContactModel> getResult = this.contactRepository.findById(id);
+        if(!getResult.isPresent()){
+            throw new ResourceNotFoundException("Contact not found");
+        }
+        ContactModel model = getResult.get();
         contactMapperService.updateContactFromEntity(contact, model);
         ContactModel result = this.contactRepository.save(model);
         return ContactMapper.convertModelToEntity(result);
